23e99fe666e446d73a9a48e31d7ff67adfb275f1
Unknown <nicolas.faqir@hotmail.fr>
2017-02-27 16:29:51 +0100

    Add - Starter

---
+++ app/src/main/java/com/ingesup/nicolas/foodmonitor/Food.java
@@ -0,0 +1,42 @@
+package com.ingesup.nicolas.foodmonitor;
+
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Nicolas on 27/02/2017.
+ */
+
+public class Food {
+    private String name;
+    private Date expDate;
+    public static ArrayList<Food> listFood = new ArrayList<Food>() ;
+
+    Food (String name, Date expDate){
+        this.name = name;
+        this.expDate = expDate;
+        listFood.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getExpDate(){
+        return expDate;
+    }
+
+    public static List<String> getListNameFoodList(){
+        List<String> names = new ArrayList<String>() {};
+
+        for (Food food:listFood
+             ) {
+            names.add(food.name);
+        }
+
+        return names;
+    }
+}
---
+++ app/src/main/java/com/ingesup/nicolas/foodmonitor/Principale.java
@@ -0,0 +1,81 @@
+package com.ingesup.nicolas.foodmonitor;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Date;
+
+public class Principale extends AppCompatActivity {
+
+    ListView mListView;
+
+    Date date1 = new Date(2017,02,28);
+    Date date2 = new Date(2017,02,29);
+
+    Food food1 = new Food("pate",date1);
+    Food food2 = new Food("cereales",date2);
+
+
+
+    String[] values = new String[]{
+
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principale);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        mListView = (ListView) findViewById(R.id.listFood);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, Food.getListNameFoodList());
+
+
+
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_principale, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
